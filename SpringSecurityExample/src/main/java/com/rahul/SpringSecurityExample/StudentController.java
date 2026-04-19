7f28d3edfdc75a1247e7ef92f464adc176b831aa
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-04-19 10:04:19 +0600

    added csrf get mapping

--- SpringSecurityExample/src/main/java/com/rahul/SpringSecurityExample/StudentController.java
+++ SpringSecurityExample/src/main/java/com/rahul/SpringSecurityExample/StudentController.java
@@ -3,10 +3,13 @@ package com.rahul.SpringSecurityExample;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 public class StudentController {
@@ -21,6 +24,11 @@ public class StudentController {
     @GetMapping("/students")
     public List<Student> getStudents() {
         return students;
+    }
+
+    @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request ){
+        return (CsrfToken) request.getAttribute("_csrf");
     }
 
     @PostMapping("/students")
