51cbaaa8635051d845358e589cafd3fb659c212e
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-04-19 11:27:07 +0600

    Add UserDetailsService bean for in-memory user management in SecurityConfig

--- SpringSecurityExample/src/main/java/com/rahul/SpringSecurityExample/config/SecurityConfig.java
+++ SpringSecurityExample/src/main/java/com/rahul/SpringSecurityExample/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,5 +24,10 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager();
     }
 }
