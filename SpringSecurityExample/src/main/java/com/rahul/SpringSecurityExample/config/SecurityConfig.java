deff4ced162de8d4c4a774e90910ee92b4a15f7e
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-04-19 11:32:51 +0600

    modify UserDetailsService to include in-memory user details for authentication with default user

--- SpringSecurityExample/src/main/java/com/rahul/SpringSecurityExample/config/SecurityConfig.java
+++ SpringSecurityExample/src/main/java/com/rahul/SpringSecurityExample/config/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.rahul.SpringSecurityExample.config;
 
+import org.springframework.boot.security.autoconfigure.SecurityProperties.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -28,6 +30,22 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
-        return new InMemoryUserDetailsManager();
+
+        UserDetails user1 = User
+                .withDefaultPasswordEncoder ()
+                .username("rahul")
+                .password("rahul123")
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User
+                .withDefaultPasswordEncoder ()
+                .username("admin")
+                .password("admin123")
+                .roles("USER", "ADMIN")
+                .build();
+
+
+        return new InMemoryUserDetailsManager(user1 , user2);
     }
 }
